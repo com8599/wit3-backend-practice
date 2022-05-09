4dd28ad1673ff8c5664dff6f16eb7aed82763fab
jmk307 <jimin112688@gmail.com>
2022-05-09 11:23:34 +0900

    printStar 방법 2 추가

--- 강지민/printStar.java
+++ 강지민/printStar.java
@@ -1,10 +1,18 @@
 public class printStar {
     public static void main(String[] args) {
-
+        // 방법 1
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i < 11; i++) {
             sb.append("*");
             System.out.println(sb);
+        }
+
+        // 방법 2
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
