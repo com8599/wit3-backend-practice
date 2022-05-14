84aee9add1f71a8f2d64db0953901d78430045dd
xeangit <mily072@naver.com>
2022-05-14 12:32:41 +0900

    week2 과제 제출

---
+++ 박시연/week2_2.java
@@ -0,0 +1,28 @@
+public class week2_2 {
+
+    public static void printstar() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 3; i >= 1; i--) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printstar();
+    }
+
+}
