96907ae175949dedd4282ed625721e5b8e076e03
xeangit <mily072@naver.com>
2022-05-23 20:28:47 +0900

    week4 과제 제출

---
+++ 박시연/week3.java
@@ -0,0 +1,41 @@
+public class week3 {
+
+
+    public static void printstart() {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void printstar() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 3; i >= 1; i--) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        printstar();
+    }
+}
+
+class 
