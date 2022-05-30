955aeae9f9b8bc00ba1457dab8ff22f4a073856b
xeangit <mily072@naver.com>
2022-05-30 22:31:47 +0900

    chore: Add member crud

--- 박시연/week4/demo/src/main/java/com/example/demo/controller/MemberApiController.java
+++ 박시연/week4/demo/src/main/java/com/example/demo/controller/MemberApiController.java
@@ -1,11 +1,46 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberApiController {
+    @Autowired
     private final MemberService memberService;
+
+
+    @GetMapping("")
+    public List<Member> getAllMembers(){
+        return memberService.getAllMembers();
+    }
+
+    @GetMapping("/{id}")
+    public Member getMemberByMemberId(@PathVariable String id){
+        return MemberService.getMemberByMemberId(id);
+    }
+
+    @PostMapping("")
+    public Member registerMember(@RequestBody Member member){
+        return MemberService.registerMember(member);
+    }
+
+    @PutMapping("/{id}")
+    public void modifyMember(@PathVariable String id, @RequestBody Member Member) {
+        MemberService.modifyMember(id, member);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeMember(@PathVariable String id){
+        memberService.removeMember(id);
+    }
+
+
+
 }
