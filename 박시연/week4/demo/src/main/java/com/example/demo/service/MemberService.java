76a7ce9473d8f6231173ba482aab172596246a22
xeangit <mily072@naver.com>
2022-06-01 21:17:27 +0900

    chore: Add member service

--- 박시연/week4/demo/src/main/java/com/example/demo/service/MemberService.java
+++ 박시연/week4/demo/src/main/java/com/example/demo/service/MemberService.java
@@ -1,11 +1,40 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    @Autowired
+    private final MemberService memberService;
+
+
+    public static Member getMemberByMemberId(String id) {
+        return MemberService.getMemberByMemberId(id);
+    }
+
+    public static Member registerMember(Member member) {
+        return MemberService.registerMember(member);
+    }
+
+    public List<Member> getAllMembers() {
+        return memberService.getAllMembers();
+    }
+
+    public void removeMember(){
+        return memberService.removeMember();
+    }
+
+    public void modifyMember() {
+        MemberService.modifyMember();
+    }
 }
