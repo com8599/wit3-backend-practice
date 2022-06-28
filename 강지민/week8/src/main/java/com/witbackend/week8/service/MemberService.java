13d7ce074f91eb25a29faf163900f92f87681667
jmk307 <jimin112688@gmail.com>
2022-06-29 02:05:29 +0900

    refactor : 필요없는 과정 없앰

--- 강지민/week8/src/main/java/com/witbackend/week8/service/MemberService.java
+++ 강지민/week8/src/main/java/com/witbackend/week8/service/MemberService.java
@@ -1,14 +1,15 @@
 package com.witbackend.week8.service;
 
 import com.witbackend.week8.domain.Member;
-import com.witbackend.week8.dto.MemberDTO.MemberRequestDTO;
-import com.witbackend.week8.dto.MemberDTO.MemberResponseDTO;
-import com.witbackend.week8.dto.MemberDTO.MemberUpdateRequestDto;
+import com.witbackend.week8.dto.MemberDto.MemberRequestDto;
+import com.witbackend.week8.dto.MemberDto.MemberResponseDto;
+import com.witbackend.week8.dto.MemberDto.MemberUpdateRequestDto;
 import com.witbackend.week8.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,37 +18,31 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     // RequestDTO -> Entity로 변환
-    public MemberResponseDTO register(MemberRequestDTO memberRequestDTO) {
+    public MemberResponseDto register(MemberRequestDto memberRequestDTO) {
         Member member = memberRequestDTO.toEntity();
         memberRepository.save(member);
-        return new MemberResponseDTO(member);
+        return new MemberResponseDto(member);
     }
 
     // List<Member> -> List<MemberResponseDTO>로 변환
-    public List<MemberResponseDTO> findMembers() {
-        List<Member> memberList = memberRepository.findAll();
-        List<MemberResponseDTO> dtoList = new ArrayList<>();
-
-        for (Member member : memberList) {
-            dtoList.add(new MemberResponseDTO(member));
-        }
-        return dtoList;
-        // return memberList.stream().map(MemberResponseDTO::new).collect(Collectors.toList());
+    public List<MemberResponseDto> findMembers(int page, Pageable pageable) {
+        List<MemberResponseDto> memberList = memberRepository.findAllBy(PageRequest.of(page, 3));
+        return memberList;
     }
 
-    public MemberResponseDTO findOne(Long id) {
+    public MemberResponseDto findOne(Long id) {
         Member member = memberRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
-        return new MemberResponseDTO(member);
+        return new MemberResponseDto(member);
     }
 
-    public MemberResponseDTO updateMember(Long id, MemberUpdateRequestDto memberUpdateRequestDto) {
+    public MemberResponseDto updateMember(Long id, MemberUpdateRequestDto memberUpdateRequestDto) {
         Member member = memberRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         member.update(memberUpdateRequestDto.getEmail(), memberUpdateRequestDto.getPassword());
 
         memberRepository.save(member);
 
-        return new MemberResponseDTO(member);
+        return new MemberResponseDto(member);
     }
 
     public void delete(Long id) {
