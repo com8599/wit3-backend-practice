894c29933c5bf428f777e4021cb4b9828fe042c5
jmk307 <jimin112688@gmail.com>
2022-06-29 02:03:36 +0900

    refactor : 리다이렉션 없앰, 페이징 추가

--- 강지민/week8/src/main/java/com/witbackend/week8/controller/api/MemberApiController.java
+++ 강지민/week8/src/main/java/com/witbackend/week8/controller/api/MemberApiController.java
@@ -1,16 +1,15 @@
 package com.witbackend.week8.controller.api;
 
-import com.witbackend.week8.dto.MemberDTO.MemberRequestDTO;
-import com.witbackend.week8.dto.MemberDTO.MemberResponseDTO;
-import com.witbackend.week8.dto.MemberDTO.MemberUpdateRequestDto;
+import com.witbackend.week8.dto.MemberDto.MemberRequestDto;
+import com.witbackend.week8.dto.MemberDto.MemberResponseDto;
+import com.witbackend.week8.dto.MemberDto.MemberUpdateRequestDto;
 import com.witbackend.week8.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,13 +21,13 @@ public class MemberApiController {
 
     // 목록 GET
     @GetMapping
-    public ResponseEntity<List<MemberResponseDTO>> list() {
-        return new ResponseEntity<>(memberService.findMembers(), HttpStatus.OK);
+    public ResponseEntity<List<MemberResponseDto>> list(@RequestParam int page, Pageable pageable) {
+        return new ResponseEntity<>(memberService.findMembers(page, pageable), HttpStatus.OK);
     }
 
     // 수정 GET
     @GetMapping("{id}")
-    public ResponseEntity<MemberResponseDTO> mod(@PathVariable Long id) {
+    public ResponseEntity<MemberResponseDto> mod(@PathVariable Long id) {
         if (memberService.findOne(id) == null) {
             return ResponseEntity.notFound().build();
         }
@@ -37,18 +36,14 @@ public class MemberApiController {
 
     // 등록 POST
     @PostMapping
-    public ResponseEntity<MemberResponseDTO> postAdd(@RequestBody MemberRequestDTO memberRequestDTO) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(URI.create(REDIRECT_TO_LIST));
-        return new ResponseEntity<>(memberService.register(memberRequestDTO), headers, HttpStatus.SEE_OTHER);
+    public ResponseEntity<MemberResponseDto> postAdd(@RequestBody MemberRequestDto memberRequestDTO) {
+        return new ResponseEntity<>(memberService.register(memberRequestDTO), HttpStatus.OK);
     }
 
     // 수정 PUT
     @PutMapping("{id}")
-    public ResponseEntity<MemberResponseDTO> putMod(@RequestBody MemberUpdateRequestDto memberUpdateRequestDto, @PathVariable Long id) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(URI.create(REDIRECT_TO_LIST));
-        return new ResponseEntity<>(memberService.updateMember(id, memberUpdateRequestDto), headers, HttpStatus.SEE_OTHER);
+    public ResponseEntity<MemberResponseDto> putMod(@RequestBody MemberUpdateRequestDto memberUpdateRequestDto, @PathVariable Long id) {
+        return new ResponseEntity<>(memberService.updateMember(id, memberUpdateRequestDto), HttpStatus.OK);
     }
 
     // 삭제 DELETE
