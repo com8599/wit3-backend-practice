f2b84572c907d5ae4de45397080afa6273273ec8
jmk307 <jimin112688@gmail.com>
2022-06-29 02:05:10 +0900

    feat : 페이징을 위해 findAllBy(Pageable pageable) 추가

--- 강지민/week8/src/main/java/com/witbackend/week8/repository/MemberRepository.java
+++ 강지민/week8/src/main/java/com/witbackend/week8/repository/MemberRepository.java
@@ -1,8 +1,13 @@
 package com.witbackend.week8.repository;
 
 import com.witbackend.week8.domain.Member;
+import com.witbackend.week8.dto.MemberDto.MemberResponseDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    List<MemberResponseDto> findAllBy(Pageable pageable);
 }
