86e37043258a1a0cc5f24646bef7eb2372833f3e
xeangit <mily072@naver.com>
2022-07-01 20:43:49 +0900

    refactor: Add pagination

--- 박시연/week4/demo/src/main/java/com/example/demo/controller/MemberApiController.java
+++ 박시연/week4/demo/src/main/java/com/example/demo/controller/MemberApiController.java
@@ -4,10 +4,20 @@ import com.example.demo.controller.dto.MemberResponseDto;
 import com.example.demo.controller.dto.MemberSaveRequestDto;
 import com.example.demo.controller.dto.MemberUpdateRequestDto;
 import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
 import com.example.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.tuple.InMemoryValueGenerationStrategy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.Response;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -35,16 +45,15 @@ public class MemberApiController {
 
     //1. id 특정해서 한개만 읽기
     @GetMapping(value = "/{id}")
-    public MemberResponseDto readMember(@PathVariable Long id) {
-        return memberService.readOne(id);
+    public ResponseEntity<MemberResponseDto> readMember(@PathVariable Long id) {
+        return new ResponseEntity<>(memberService.readOne(id), HttpStatus.OK);
     }
 
     //2. 전부다 읽기
     //리스트 사용
     @GetMapping()
-    public List<Member> readAllMembers(){
-        List<Member> members = memberService.readAll();
-        return members;
+    public ResponseEntity<List<MemberResponseDto>> readAllMembers(@RequestParam int page, Pageable pageable){
+        return new ResponseEntity<>(memberService.findMembersByPageRequest(pageable),HttpStatus.OK);
     }
 
 
@@ -52,17 +61,18 @@ public class MemberApiController {
 
     //특정하는 작업 먼저 하고
     @PutMapping("/{id}")
-    public String updateMember(@PathVariable Long id, @RequestBody MemberUpdateRequestDto requestDto){
-        memberService.update(id, requestDto);
-        return "update";
+    public ResponseEntity updateMember(@PathVariable Long id, @RequestBody MemberUpdateRequestDto requestDto){
+        return new ResponseEntity<>( memberService.update(id, requestDto), HttpStatus.OK);
     }
 
 
     //DELETE
 
     //id로 어떤거 지울지 특정
-    public void deleteMember(@PathVariable Long id){
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteMember(@PathVariable Long id){
         memberService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
--- 박시연/week4/demo/src/main/java/com/example/demo/repository/MemberRepository.java
+++ 박시연/week4/demo/src/main/java/com/example/demo/repository/MemberRepository.java
@@ -1,8 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.controller.dto.MemberResponseDto;
 import com.example.demo.domain.Member;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     //DB에 접근하는 모든 코드
+    List<MemberResponseDto> findAllBy(Pageable pageable);
 }
