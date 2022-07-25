3ae6d72670f7dd51699eb5f4f2374c91106dc02c
jmk307 <jimin112688@gmail.com>
2022-07-25 10:34:53 +0900

    chore :

--- 강지민/week8/src/main/java/com/witbackend/week8/service/MemberService.java
+++ 강지민/week8/src/main/java/com/witbackend/week8/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.witbackend.week8.service;
 
 import com.witbackend.week8.domain.Member;
+import com.witbackend.week8.domain.SearchCondition;
 import com.witbackend.week8.dto.MemberDto.MemberRequestDto;
 import com.witbackend.week8.dto.MemberDto.MemberResponseDto;
 import com.witbackend.week8.dto.MemberDto.MemberUpdateRequestDto;
+import com.witbackend.week8.repository.MemberCustomRepository;
 import com.witbackend.week8.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -11,11 +13,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final MemberCustomRepository memberCustomRepository;
 
     // RequestDTO -> Entity로 변환
     public MemberResponseDto register(MemberRequestDto memberRequestDTO) {
@@ -27,6 +31,12 @@ public class MemberService {
     // List<Member> -> List<MemberResponseDTO>로 변환
     public List<MemberResponseDto> findMembers(int page, Pageable pageable) {
         return memberRepository.findAllBy(PageRequest.of(page, 3));
+    }
+
+    // 검색 포함 목록 보기
+    public List<MemberResponseDto> findSearchMembers(int page, Pageable pageable, SearchCondition condition) {
+        List<Member> search = memberCustomRepository.search(condition, PageRequest.of(page, 3));
+        return search.stream().map(MemberResponseDto::new).collect(Collectors.toList());
     }
 
     public MemberResponseDto findOne(Long id) {
